70abdaaf125a0354e8f064a4b15ccbc8314d2061
Hussein Awala <houssein.awala.96@gmail.com>
2022-10-19 00:30:11 +0200

    Project refacto (#2)
    In this commit, I:
    - upgrade default version to 2.4.1
    - rename docker services and move the yml file to a folder in order to add new services
    - add some invoke tasks to simplfy interacting with docker compose
    - add pre-commit to apply linters on each commit and run them on all the files
    - update readme file

--- docker-compose/airflow/scripts/entrypoint.sh
+++ docker-compose/airflow/scripts/entrypoint.sh
@@ -4,7 +4,7 @@ airflow db init
 airflow users create \
        --username "${_AIRFLOW_WWW_USER_USERNAME="admin"}" \
        --firstname "${_AIRFLOW_WWW_USER_FIRSTNAME="Airflow"}" \
-       --lastname "${_AIRFLOW_WWW_USER_LASTNME="Admin"}" \
+       --lastname "${_AIRFLOW_WWW_USER_LASTNAME="Admin"}" \
        --email "${_AIRFLOW_WWW_USER_EMAIL="airflowadmin@example.com"}" \
        --role "${_AIRFLOW_WWW_USER_ROLE="Admin"}" \
        --password "${_AIRFLOW_WWW_USER_PASSWORD}" || true
